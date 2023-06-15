de70edb7082f8c1326a3af65010b738d48c22bb6
cedras <micced1@gmail.com>
2023-06-16 01:34:18 +0200

    add alarm scheduler

---
+++ app/src/main/java/com/example/greenhillhotel/ui/book/AlarmReceiver.java
@@ -0,0 +1,13 @@
+package com.example.greenhillhotel.ui.book;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Tutaj możesz wyświetlić powiadomienie lub wykonać inne działania
+        // w momencie otrzymania zaplanowanego alarmu
+    }
+}
