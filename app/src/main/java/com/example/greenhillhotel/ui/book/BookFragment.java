b09fb67efee5fcb19732751ef15679940109abd3
cedras <micced1@gmail.com>
2023-06-15 17:06:48 +0200

    add search design

--- app/src/main/java/com/example/greenhillhotel/ui/book/BookFragment.java
+++ app/src/main/java/com/example/greenhillhotel/ui/book/BookFragment.java
@@ -1,20 +1,104 @@
 package com.example.greenhillhotel.ui.book;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.NumberPicker;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.greenhillhotel.MainActivity;
 import com.example.greenhillhotel.R;
 
+import java.util.Calendar;
+
 public class BookFragment extends Fragment {
+
+    EditText date;
+    EditText date2;
+    DatePickerDialog datePickerDialog;
+    DatePickerDialog datePickerDialog2;
+    private NumberPicker picker1;
+    private String[] pickerVals;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_book, container, false);
+        date = (EditText) view.findViewById(R.id.date);
+        date2 = (EditText) view.findViewById(R.id.date2);
+        // perform click event on edit text
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR); // current year
+                int mMonth = c.get(Calendar.MONTH); // current month
+                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+                // date picker dialog
+                datePickerDialog = new DatePickerDialog(getActivity(),
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // set day of month , month and year value in the edit text
+                                date.setText(dayOfMonth + "/"
+                                        + (monthOfYear + 1) + "/" + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+
+                datePickerDialog.show();
+                datePickerDialog.getDatePicker().setDescendantFocusability(DatePicker.FOCUS_BLOCK_DESCENDANTS);
+
+            }
+        });
+        date2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR); // current year
+                int mMonth = c.get(Calendar.MONTH); // current month
+                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+                // date picker dialog
+                datePickerDialog2 = new DatePickerDialog(getActivity(),
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // set day of month , month and year value in the edit text
+                                date2.setText(dayOfMonth + "/"
+                                        + (monthOfYear + 1) + "/" + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+
+                datePickerDialog2.show();
+                datePickerDialog2.getDatePicker().setDescendantFocusability(DatePicker.FOCUS_BLOCK_DESCENDANTS);
+
+            }
+        });
+
+        picker1 = view.findViewById(R.id.picker);
+        picker1.setMaxValue(2);
+        picker1.setMinValue(0);
+        pickerVals  = new String[] {"1", "2", "3"};
+        picker1.setDisplayedValues(pickerVals);
+        picker1.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                int valuePicker1 = picker1.getValue();
+                Log.d("picker value", pickerVals[valuePicker1]);
+            }
+        });
+
 
 
 
