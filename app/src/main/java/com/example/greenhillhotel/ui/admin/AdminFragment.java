a65bc8f138351f5dd7434b56b4c20a96cd327423
jjay31 <michalwojtowicz_2001@tlen.pl>
2023-06-16 00:20:44 +0200

    fix admin panel

--- app/src/main/java/com/example/greenhillhotel/ui/admin/AdminFragment.java
+++ app/src/main/java/com/example/greenhillhotel/ui/admin/AdminFragment.java
@@ -25,14 +25,13 @@ public class AdminFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_admin, container, false);
-        Spinner spinner2 = view.findViewById(R.id.spinner2);
+        Spinner spinner3 = view.findViewById(R.id.spinner3);
 
         String[] items = new String[]{"1", "2", "3"};
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_dropdown_item, items);
 //set the spinners adapter to the previously created one.
-        spinner2.setAdapter(adapter);
-
+        spinner3.setAdapter(adapter);
         return view;
     }
 }
