20b7112738f00ada56f77a72fc8c283b73ee4f3c
cedras <micced1@gmail.com>
2023-06-15 20:07:34 +0200

    add admin panel design

--- app/src/main/java/com/example/greenhillhotel/ui/admin/AdminFragment.java
+++ app/src/main/java/com/example/greenhillhotel/ui/admin/AdminFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -23,8 +25,13 @@ public class AdminFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_admin, container, false);
+        Spinner spinner2 = view.findViewById(R.id.spinner2);
 
+        String[] items = new String[]{"1", "2", "3"};
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_dropdown_item, items);
+//set the spinners adapter to the previously created one.
+        spinner2.setAdapter(adapter);
 
         return view;
     }
