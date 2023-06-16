ba04398c1bb95c1fc0633db5eec53932b4a01914
jjay31 <michalwojtowicz_2001@tlen.pl>
2023-06-16 03:29:44 +0200

    add RoomCreator.java javadoc

--- app/src/main/java/com/example/greenhillhotel/RoomCreator.java
+++ app/src/main/java/com/example/greenhillhotel/RoomCreator.java
@@ -14,7 +14,16 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * Class used only once to generate
+ * rooms and put them into firebase.
+ */
 public class RoomCreator {
+    /**
+     * Method to generate 80 rooms with random attributes
+     * and to put them into firebase with proper
+     * fields and values.
+     */
     public static void createRooms() {
         System.out.println("Inserting records into the table...");
         FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -29,12 +38,18 @@ public class RoomCreator {
             db.collection("rooms").document(String.format("%02d", i))
                     .set(roomData)
                     .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        /**
+                         * Method to log successful room creation.
+                         */
                         @Override
                         public void onSuccess(Void aVoid) {
                             Log.d(TAG, "DocumentSnapshot added with ID: " + finalI);
                         }
                     })
                     .addOnFailureListener(new OnFailureListener() {
+                        /**
+                         * Method to log unsuccessful room creation.
+                         */
                         @Override
                         public void onFailure(@NonNull Exception e) {
                             Log.w(TAG, "Error adding document", e);
